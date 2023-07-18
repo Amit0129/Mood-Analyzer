d3d74b30539784a470254c11ff19f1af2b82a54c
Amit Kumar Nayak <amitkumarnayak40@gmail.com>
2023-07-18 23:43:04 +0530

    [Amit]Add : Given I am in Any mood message should return HAPPY

--- MoodAnalyser/MoodAnalyser_Test/MoodAnalyse_Test.cs
+++ MoodAnalyser/MoodAnalyser_Test/MoodAnalyse_Test.cs
@@ -6,11 +6,19 @@ namespace MoodAnalyser_Test
     public class MoodAnalyse_Test
     {
         [TestMethod]
-        public void GivenSadMood_ShouldReturnSad()
+        public void GivenSadMood_ShouldReturnSAD()
         {
             MoodAnalyserBuilder moodAnalyser = new MoodAnalyserBuilder();
             string result = moodAnalyser.AnalyseMood("I am in Sad Mood");
             string expexted = "SAD";
+            Assert.AreEqual(result, expexted);
+        }
+        [TestMethod]
+        public void GivenaNYMood_ShouldReturnHAPPY()
+        {
+            MoodAnalyserBuilder moodAnalyser = new MoodAnalyserBuilder();
+            string result = moodAnalyser.AnalyseMood("I am in Any Mood");
+            string expexted = "HAPPY";
             Assert.AreEqual(result, expexted);
         }
     }
