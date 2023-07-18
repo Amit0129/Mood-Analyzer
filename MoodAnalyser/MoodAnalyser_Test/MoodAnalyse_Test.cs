f412624ccf490495ddb2771bd38b833699bb7cb4
Amit Kumar Nayak <amitkumarnayak40@gmail.com>
2023-07-19 00:06:41 +0530

    [Amit]Add : Given I am in Happy mood message in constructor should return HAPPY

--- MoodAnalyser/MoodAnalyser_Test/MoodAnalyse_Test.cs
+++ MoodAnalyser/MoodAnalyser_Test/MoodAnalyse_Test.cs
@@ -12,5 +12,12 @@ namespace MoodAnalyser_Test
             string result = moodAnalyser.AnalyseMood();
             Assert.AreEqual(result, "SAD");
         }
+        [TestMethod]
+        public void GivenHappyMoodInConstrutor_ShouldReturnHappy()
+        {
+            MoodAnalyserBuilder moodAnalyser = new MoodAnalyserBuilder("I am in Happy Mood");
+            string result = moodAnalyser.AnalyseMood();
+            Assert.AreEqual(result, "HAPPY");
+        }
     }
 }
